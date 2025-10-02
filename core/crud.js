3b2e548f31776604e39deb012a592c26d9036b35
diego-rng <diego.renova55@gmail.com>
2025-10-02 16:27:54 -0300

    progress to 2.1

---
+++ core/crud.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const DB_FILE_PATH = "./core/db";
+console.log("[CRUD]");
+
+
+function create(content) {
+    // attempt save system
+    fs.writeFileSync(DB_FILE_PATH, content);
+    return content; 
+}
+
+// [SIMULATION]
+console.log(create("Test 2"));
